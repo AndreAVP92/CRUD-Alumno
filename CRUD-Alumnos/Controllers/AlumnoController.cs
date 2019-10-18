9c550b9abb509fbadb4e3ed8b7febc7972c1ade4
André Villalta <andre_acdc92@live.com>
2019-10-18 14:17:20 -0300

    se creó la  función Agregar Alumno

---
+++ CRUD-Alumnos/Controllers/AlumnoController.cs
@@ -0,0 +1,65 @@
+﻿using CRUD_Alumnos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_Alumnos.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            try
+            {
+                using (var db = new AlumnosContext()) //para no dejar la conexion de DB libre
+                {
+                    return View(db.Alumno.ToList());  //ME MUESTRA UNA LISTA DE ALUMNOS
+                                                      //Un ejemplo: List<Alumno> lista = db.Alumno.Where(a => a.Edad > 18).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+               
+            
+        }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost] //PARA QUE RECIBA LA ENTIDAD ALUMNO
+        [ValidateAntiForgeryToken] //GOOGLEAR, SON DETALLITOS
+        public ActionResult Agregar(Alumno a)
+        {
+            
+            if (!ModelState.IsValid)//si el nombre que entra no es válido...
+                    return View();
+            try
+            {
+                
+
+                using (AlumnosContext db = new AlumnosContext()) ///Para abrir y cerrar la conexion
+                {
+                    a.FechaRegistro = DateTime.Now;
+
+                    db.Alumno.Add(a);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("","Error Registro de Alumno - " + ex.Message);
+                return View();
+            }           
+        }
+    }
+}
---
+++ CRUD-Alumnos/Models/AlumnoCE.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_Alumnos.Models
+{
+    public class AlumnoCE
+    {
+        [Required]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+        [Required]
+        [Display(Name = "Apellido")]
+        public string Apellido { get; set; }
+        [Required]
+        [Display(Name = "Edad")]
+        public int Edad { get; set; }
+        [Required]
+        [Display(Name = "Sexo")]
+        public string Sexo { get; set; }
+    }
+
+    [MetadataType(typeof(AlumnoCE))]
+    public partial class Alumno
+    {
+        //public int Estado { get; set; }
+        public string NombreCompleto { get { return Nombre + " " + Apellido;} }
+    }
+}
